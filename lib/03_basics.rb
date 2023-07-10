5e46a53a2ff9392367595d9ca8390ae94a7c21f8
doumsdd <65906981+doumsdd@users.noreply.github.com>
2023-07-11 01:42:50 +0200

    Update 03_basics.rb

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,1 +1,19 @@
+describe 'crazy stuff on strings' do
+  it 'does crazy stuff on strings' do
+    expect(reverse_upcase_noLTA("Tries this at Home, Kids"))
+      .to eq("SDIK ,EMOH  SIH SEIR")
+  end
+end
 
+describe 'crazy stuff on arrays' do
+  it 'does crazy stuff on Arrays' do
+    expect(magic_array([1, 2, 3, 4, 5, 6]))
+      .to eq([2, 4, 8, 10])
+  end
+end
+
+describe '42 finder' do
+  it 'finds 42' do
+    expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])).to eq false
+  end
+end
