e7708735119b30df006cca6b823ef375952532ab
doumsdd <65906981+doumsdd@users.noreply.github.com>
2023-07-11 01:39:19 +0200

    Update 02_calculator.rb

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,1 +1,11 @@
+describe "#power" do
+  it "raises one number to the power of another number" do
+    expect(power(2, 3)).to eq(8)
+  end
+end
 
+#describe "#multiply" do
+  #it "multiplies two other numbers, one of them being 0" do
+    #expect(multiply(8, 0)).to eq(0)
+  #end
+#end
