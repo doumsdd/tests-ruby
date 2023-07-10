88b210a631016e4cf147d6e05e8ed1e5fd878410
doumsdd <65906981+doumsdd@users.noreply.github.com>
2023-07-11 01:51:19 +0200

    Update 02_calculator.rb

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,11 +1,16 @@
-describe "#power" do
-  it "raises one number to the power of another number" do
-    expect(power(2, 3)).to eq(8)
+def multiply(a, b)
+  a * b
+end
+
+def power(base, exponent)
+  base ** exponent
+end
+
+def factorial(n)
+  if n == 0
+    1
+  else
+    (1..n).reduce(:*)
   end
 end
 
-#describe "#multiply" do
-  #it "multiplies two other numbers, one of them being 0" do
-    #expect(multiply(8, 0)).to eq(0)
-  #end
-#end
