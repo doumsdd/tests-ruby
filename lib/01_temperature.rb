7cf084cf79494efdc6c7b1b57a8f1ef404868eed
doumsdd <65906981+doumsdd@users.noreply.github.com>
2023-07-11 01:31:49 +0200

    Update 01_temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,1 +1,13 @@
+describe "temperature conversion functions" do
+  describe "#ftoc" do
+    it "converts body temperature" do
+      expect(ftoc(98.6)).to eq(37)
+    end
+  end
 
+  describe "#ctof" do
+    it "converts body temperature" do
+      expect(ctof(37)).to be_within(0.1).of(98.6)
+    end
+  end
+end
